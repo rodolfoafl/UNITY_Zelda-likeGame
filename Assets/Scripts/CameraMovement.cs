46eae5436281e8640b6b4333a7eb25130d1743e1
Rodolfo <rodolfoafl@gmail.com>
2019-02-04 17:58:29 -0200

    Added new features
    Implemented camera movement to follow player, limited by level size;

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    [SerializeField] Transform _target;
+
+    [SerializeField] float _smoothing;
+
+    [SerializeField] Vector2 _maxPosition;
+    [SerializeField] Vector2 _minPosition;
+
+    void LateUpdate()
+    {
+        if(transform.position != _target.position)
+        {
+            Vector3 targetPosition = new Vector3(_target.position.x, _target.position.y, transform.position.z);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, _minPosition.x, _maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, _minPosition.y, _maxPosition.y);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, _smoothing);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] float _speed;
+
+    Rigidbody2D _rigidbody;
+
+    Vector3 _change;
+
+    Animator _animator;
+
+
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+	
+	void Update () {
+        _change = Vector2.zero;
+        _change.x = Input.GetAxisRaw("Horizontal");
+        _change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+	}
+
+    void UpdateAnimationAndMove()
+    {
+        if (_change != Vector3.zero)
+        {
+            MoveCharacter();
+            _animator.SetFloat("moveX", _change.x);
+            _animator.SetFloat("moveY", _change.y);
+            _animator.SetBool("moving", true);
+        }
+        else
+        {
+            _animator.SetBool("moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        _rigidbody.MovePosition(transform.position + _change.normalized * _speed * Time.deltaTime);
+    }
+}
