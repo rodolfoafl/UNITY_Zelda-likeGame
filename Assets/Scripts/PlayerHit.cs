c1e0836258c89bc51dd01467d00cee24893e4ff9
Rodolfo <rodolfoafl@gmail.com>
2019-02-05 17:49:54 -0200

    New feature
    Implemented object breaking when hit by player;

---
+++ Assets/Scripts/PlayerHit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHit : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("OnTriggerEnter2D");
+        if (other.CompareTag("Breakable"))
+        {
+            if (other.GetComponent<Pot>() != null)
+            {
+                other.GetComponent<Pot>().Break();
+            }
+            else
+            {
+                Debug.Log("Hitting an unbreakable object!");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Pot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour {
+
+    Animator _animator;
+
+    [SerializeField] float _deactivationTime;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Break()
+    {
+        _animator.SetBool("break", true);
+        StartCoroutine(DeactivateObject());
+    }
+
+    IEnumerator DeactivateObject()
+    {
+        yield return new WaitForSeconds(_deactivationTime);
+        gameObject.SetActive(false);
+    }
+}
