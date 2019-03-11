d22b9470c26469910c268aecd76086ed4715d69a
Rodolfo Leal <rodolfoafl@gmail.com>
2019-03-11 01:43:00 -0300

    New features
    Implemented new type of Enemy: Turret (launches a projectile to hit Player)

---
+++ Assets/Scripts/Enemies/TurretLog.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZeldaTutorial.Enemies;
+
+public class TurretLog : Log {
+
+	[SerializeField] GameObject _projectilePrefab;
+    [SerializeField] float _fireDelay;
+    float _fireDelaySeconds;
+    bool _canFire = true;
+
+    void Update()
+    {
+        _fireDelaySeconds -= Time.deltaTime;
+        if(_fireDelaySeconds <= 0)
+        {
+            _canFire = true;
+            _fireDelaySeconds = _fireDelay;
+        }
+    }
+	public override void CheckTargetDistance()
+	{
+		if(CurrentState == CharacterState.STAGGER || CurrentState == CharacterState.ATTACK)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(Target.position, transform.position) <= ChaseRadius && Vector3.Distance(Target.position, transform.position) > AttackRadius)
+        {
+            if(_canFire)
+            {
+                Vector3 distanceVector = Target.transform.position - transform.position;
+                GameObject projectile = Instantiate(_projectilePrefab, transform.position, Quaternion.identity);
+                projectile.GetComponent<Projectile>().Launch(distanceVector);
+                _canFire = false;
+                ChangeState(CharacterState.WALK);
+                Animator.SetBool("wakeUp", true);
+            }
+        }
+        else if(Vector3.Distance(Target.position, transform.position) > ChaseRadius)
+        {
+            Animator.SetBool("wakeUp", false);
+        }
+	}
+}
---
+++ Assets/Scripts/Objects/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	[Header("Attributes")]
+	[SerializeField] float _speed;
+	[SerializeField] float _lifetime;
+
+	Vector2 _directionToMove;
+	Rigidbody2D _rigidbody;
+
+	void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+	}
+	void Update()
+	{
+		_lifetime -= Time.deltaTime;
+		if(_lifetime <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	public void Launch(Vector2 initialVelocity)
+	{
+		_rigidbody.velocity = initialVelocity * _speed;
+	}
+
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.CompareTag("Player") && !other.isTrigger){
+			Destroy(gameObject);
+		}
+	}
+}
