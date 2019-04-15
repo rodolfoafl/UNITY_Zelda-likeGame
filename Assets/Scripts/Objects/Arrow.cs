4cfea5698654c89b05307be2e626a46ee13dc331
Rodolfo Leal <rodolfoafl@gmail.com>
2019-04-14 21:09:26 -0300

    Implemented Arrow class

---
+++ Assets/Scripts/Objects/Arrow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+    [SerializeField] float _speed;
+
+    Rigidbody2D _rigidbody;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Setup(Vector2 velocity, Vector3 direction)
+    {
+        _rigidbody.velocity = velocity.normalized * _speed;
+        transform.rotation = Quaternion.Euler(direction);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("RoomCollider"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("RoomCollider"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
