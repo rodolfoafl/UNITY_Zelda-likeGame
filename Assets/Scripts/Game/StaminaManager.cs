19df8b28d7650571a0cd6e9da69cf3b9da6145e6
Rodolfo Leal <rodolfoafl@gmail.com>
2019-04-17 22:29:45 -0300

    Implemented StaminaManager class

---
+++ Assets/Scripts/Game/StaminaManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ZeldaTutorial.Player;
+
+public class StaminaManager : MonoBehaviour {
+
+    [SerializeField] Slider _staminaSlider;
+    [SerializeField] PlayerMovement _player;
+
+    void Start()
+    {
+        _staminaSlider.minValue = 0;
+        _staminaSlider.maxValue = _player.MaxStamina;
+        _staminaSlider.value = _player.MaxStamina;
+    }
+
+    public void IncreaseStamina()
+    {
+        _staminaSlider.value++;
+        if(_staminaSlider.value > _staminaSlider.maxValue)
+        {
+            _staminaSlider.value = _staminaSlider.maxValue;
+        }
+    }
+
+    public void DecreaseStamina()
+    {
+        _staminaSlider.value--;
+        if (_staminaSlider.value < _staminaSlider.minValue)
+        {
+            _staminaSlider.value = _staminaSlider.minValue;
+        }
+    }
+}
