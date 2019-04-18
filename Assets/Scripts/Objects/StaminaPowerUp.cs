88d2b30384739a6d28a956acc7aafa5bcf40012f
Rodolfo Leal <rodolfoafl@gmail.com>
2019-04-17 22:29:56 -0300

    Implemented StaminaPowerUp class

---
+++ Assets/Scripts/Objects/StaminaPowerUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaPowerUp : PowerUp {
+
+    bool _enabled = false;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && !_enabled)
+        {
+            enabled = true;
+            PowerUpSignal.Raise();
+            Destroy(gameObject);
+        }
+    }
+}
