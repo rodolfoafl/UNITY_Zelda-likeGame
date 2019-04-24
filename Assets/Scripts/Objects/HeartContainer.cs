0e1cc97348860f1496c08494af66ac43c981ad1e
Rodolfo Leal <rodolfoafl@gmail.com>
2019-04-24 01:20:11 -0300

    Implemented the HeartContainer class

---
+++ Assets/Scripts/Objects/HeartContainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartContainer : PowerUp {
+
+    [SerializeField] FloatValue _heartContainer;
+    [SerializeField] FloatValue _playerHealth;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && !other.isTrigger)
+        {
+            _heartContainer.RuntimeValue += 1;
+            _playerHealth.RuntimeValue = _heartContainer.RuntimeValue * 2;
+            PowerUpSignal.Raise();
+            Destroy(gameObject);
+        }
+    }
+}
