ec491cb389b43eac68faba22fa57831e8eb9bf04
Rodolfo Leal <rodolfoafl@gmail.com>
2019-04-28 01:42:27 -0300

    Implemented StartMenu class

---
+++ Assets/Scripts/Menu/StartMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+	public void NewGame()
+    {
+        SceneManager.LoadScene("World");
+    }
+
+    public void QuitToDesktop()
+    {
+        Application.Quit();
+    }
+}
