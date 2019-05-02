d1113207bf6175a03cf4d8db41f33b3f0592f7cc
Rodolfo Leal <rodolfoafl@gmail.com>
2019-05-01 21:03:01 -0300

    Implemented the PauseMenu
    For now, the pause menu only have two buttons: Resume and Quit

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    [SerializeField] GameObject _pausePanel;
+    [SerializeField] string _mainMenu;
+
+    bool _isPaused;
+        
+    void Start()
+    {
+        _isPaused = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            ChangePause();
+        }
+    }
+
+    public void ChangePause()
+    {
+        _isPaused = !_isPaused;
+        if (_isPaused)
+        {
+            _pausePanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            _pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void QuitToMain()
+    {
+        //TODO: Add a confirmation prompt before actually quitting!
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(_mainMenu);
+    }
+}
