915c98b2f184d9c465ddc506437ed27b983b720a
Rodolfo Leal <rodolfoafl@gmail.com>
2019-05-08 20:10:01 -0300

    Implemented GameSaveManager as Singleton

---
+++ Assets/Scripts/Game/GameSaveManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSaveManager : MonoBehaviour {
+
+    public static GameSaveManager _gameSaveManager;
+
+    [SerializeField] List<ScriptableObject> _objects = new List<ScriptableObject>();
+
+    void Awake()
+    {
+        if(_gameSaveManager == null)
+        {
+            _gameSaveManager = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this);
+    }    
+}
