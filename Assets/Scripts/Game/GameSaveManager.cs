4a46724fcd2edb799a0d68f24cef3568d984ed7c
Rodolfo <rodolfoafl@gmail.com>
2019-05-14 17:30:52 -0300

    Implemented code to save/load data to/from binary file

--- Assets/Scripts/Game/GameSaveManager.cs
+++ Assets/Scripts/Game/GameSaveManager.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class GameSaveManager : MonoBehaviour {
@@ -19,5 +21,41 @@ public class GameSaveManager : MonoBehaviour {
             Destroy(this.gameObject);
         }
         DontDestroyOnLoad(this);
-    }    
+    }  
+
+    void OnEnable()
+    {
+        LoadScriptables();
+    }
+
+    void OnDisable()
+    {
+        SaveScriptables();
+    }
+    
+    public void SaveScriptables()
+    {
+        for (int i = 0; i < _objects.Count; i++)
+        {
+            FileStream file = File.Create(Application.persistentDataPath + string.Format("/{0}.dat", i));
+            BinaryFormatter formatter = new BinaryFormatter();
+            var json = JsonUtility.ToJson(_objects[i]);
+            formatter.Serialize(file, json);
+            file.Close();
+        }
+    }
+
+    public void LoadScriptables()
+    {
+        for (int i = 0; i < _objects.Count; i++)
+        {
+            if (File.Exists(Application.persistentDataPath + string.Format("/{0}.dat", i)))
+            {
+                FileStream file = File.Open(Application.persistentDataPath + string.Format("/{0}.dat", i), FileMode.Open);
+                BinaryFormatter formatter = new BinaryFormatter();
+                JsonUtility.FromJsonOverwrite((string)formatter.Deserialize(file), _objects[i]);
+                file.Close();
+            }
+        }
+    }
 }
