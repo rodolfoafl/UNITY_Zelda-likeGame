45708450205c5a1efb89cfbe8333868745f7e075
Rodolfo Leal <rodolfoafl@gmail.com>
2019-05-29 01:37:18 -0300

    Implemented new class to hold the list of Player's itens

---
+++ Assets/Scripts/Inventory/PlayerInventory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory/PlayerInventory")]
+public class PlayerInventory : ScriptableObject
+{
+    [SerializeField] List<InventoryItem> _playerInventory = new List<InventoryItem>();
+
+    #region Properties
+    public List<InventoryItem> Inventory
+    {
+        get
+        {
+            return _playerInventory;
+        }
+
+        protected set
+        {
+            _playerInventory = value;
+        }
+    }
+    #endregion
+}
