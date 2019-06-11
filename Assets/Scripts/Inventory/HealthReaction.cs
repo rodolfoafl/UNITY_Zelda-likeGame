1d31a943e9c6150bd94177b709c9176c90821312
Rodolfo Leal <rodolfoafl@gmail.com>
2019-06-10 23:22:20 -0300

    Implemented HealthReaction class

---
+++ Assets/Scripts/Inventory/HealthReaction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthReaction : MonoBehaviour {
+
+    [SerializeField] FloatValue _playerHealth;
+    [SerializeField] Signal _healthSignal;
+
+    public void React(int amountToIncrease)
+    {
+        _playerHealth.RuntimeValue += amountToIncrease;
+        _healthSignal.Raise();
+    }
+}
