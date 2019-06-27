828c1c2b3246978af273af8fe2a681e49048d025
Rodolfo Leal <rodolfoafl@gmail.com>
2019-06-26 21:07:14 -0300

    Added System.Serializable to scripts

--- Assets/Scripts/Inventory/PlayerInventory.cs
+++ Assets/Scripts/Inventory/PlayerInventory.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory/PlayerInventory")]
 public class PlayerInventory : ScriptableObject
 {
