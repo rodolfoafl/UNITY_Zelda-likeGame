4b167fae8787d681646fd3f1e725445a71d9b193
Rodolfo Leal <rodolfoafl@gmail.com>
2019-07-03 19:03:46 -0300

    Implemented PlayerHealth using the new GenericHealth class

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : GenericHealth
+{
+    [SerializeField] Signal _healthSignal;
+
+    public override void Damage(float amountToDamage)
+    {
+        base.Damage(amountToDamage);
+        MaxHealth.RuntimeValue = CurrentHealth;
+        _healthSignal.Raise();
+    }
+}
