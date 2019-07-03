0fc8a6c32aec9cf27f62bd4694f9dacf805a133c
Rodolfo Leal <rodolfoafl@gmail.com>
2019-07-03 19:03:12 -0300

    Created Generic classed to handle characters's health and damage

---
+++ Assets/Scripts/ReusableComponents/GenericDamage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[RequireComponent(typeof(Collider2D))]
+public class GenericDamage : MonoBehaviour
+{
+    [SerializeField] float _damage;
+    [SerializeField] string _otherTag;
+    [SerializeField] InternalEditorUtility _tags;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(_otherTag) && other.isTrigger)
+        {
+            GenericHealth component = other.GetComponent<GenericHealth>();
+            if (component)
+            {
+                component.Damage(_damage);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/ReusableComponents/GenericHealth.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class GenericHealth : MonoBehaviour
+{
+    [SerializeField] FloatValue _maxHealth;
+    [SerializeField] float _currentHealth;
+
+    #region Properties
+    public FloatValue MaxHealth
+    {
+        get
+        {
+            return _maxHealth;
+        }
+
+        set
+        {
+            _maxHealth = value;
+        }
+    }
+
+    public float CurrentHealth
+    {
+        get
+        {
+            return _currentHealth;
+        }
+
+        protected set
+        {
+            _currentHealth = value;
+        }
+    }
+    #endregion
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth.RuntimeValue;
+    }
+
+    public virtual void Heal(float amountToHeal)
+    {
+        _currentHealth += amountToHeal;
+        if(_currentHealth > _maxHealth.RuntimeValue)
+        {
+            _currentHealth = _maxHealth.RuntimeValue;
+        }
+    }
+
+    public virtual void FullHeal()
+    {
+        _currentHealth = _maxHealth.RuntimeValue;
+    }
+
+    public virtual void Damage(float amountToDamage)
+    {
+        _currentHealth -= amountToDamage;
+        if(_currentHealth < 0)
+        {
+            _currentHealth = 0;
+        }
+    }
+
+    public virtual void InstantDeath()
+    {
+        _currentHealth = 0;
+    }
+}
